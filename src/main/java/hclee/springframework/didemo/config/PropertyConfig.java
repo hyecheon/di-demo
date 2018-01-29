6e3342355e4298d12e345aba0065db145cf86ad8
ihyecheon <ihyecheon@gmail.com>
2018-01-29 22:27:44 +0900

    env 설정

--- src/main/java/hclee/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/hclee/springframework/didemo/config/PropertyConfig.java
@@ -1,24 +1,31 @@
 package hclee.springframework.didemo.config;
 
 import hclee.springframework.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+
     @Value("${hclee.username}")
     private String username;
     @Value("${hclee.password}")
     private String password;
     @Value("${hclee.dburl}")
     private String url;
+    @Autowired
+    private Environment env;
 
     @Bean
     public FakeDataSource fakeDataSource() {
+        System.out.println(env.getProperty("hclee_USERNAME"));
         final FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUser(username);
         fakeDataSource.setPassword(password);
